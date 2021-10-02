f162684de6a1f6a846ed933d8f73a1dfbaf0dc0f
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-02 18:17:45 +0300

    Added 3 colors

--- The Light hw 1/ViewController.swift
+++ The Light hw 1/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isLightOn: Int = 0
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    fileprivate func updateBackGround() {
+        switch isLightOn {
+        case 1: view.backgroundColor = .gray
+        case 2: view.backgroundColor = .white
+        default: view.backgroundColor = .black
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch isLightOn {
+        case 1: isLightOn = 2
+        case 2: isLightOn = 0
+        default: isLightOn = 1
+        }
+        
+        updateBackGround()
+    }
 }
 
