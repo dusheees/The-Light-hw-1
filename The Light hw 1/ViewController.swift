04d9d46a04dc4a2bcc54c66fad8a21f7af80c932
Andrey Zhilin <komplayer31@yandex.ru>
2021-10-02 17:58:58 +0300

    Initial Commit

---
+++ The Light hw 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Light hw 1
+//
+//  Created by Андрей on 02.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
